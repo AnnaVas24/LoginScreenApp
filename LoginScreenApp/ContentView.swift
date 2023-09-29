a50a281cda409fb5bf3802086e2eb5d97710ef9b
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-09-29 14:25:52 +0300

    Initial Commit

---
+++ LoginScreenApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginScreenApp
+//
+//  Created by Vasichko Anna on 29.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginScreenApp/LoginScreenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenAppApp.swift
+//  LoginScreenApp
+//
+//  Created by Vasichko Anna on 29.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
